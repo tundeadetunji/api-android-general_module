4d3d55275ebd190fdcddd147cd96720c2dfbef38
Tunde Adetunji <tundeadetunji2017@gmail.com>
2023-06-25 17:14:01 +0100

    Add basic network features

---
+++ GeneralModule/src/main/java/com/inovationware/generalmodule/Network.java
@@ -0,0 +1,69 @@
+package com.inovationware.generalmodule;
+
+import android.app.Activity;
+import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class Network {
+
+
+    private final Activity activity;
+    private final TextView view;
+    private final String url;
+
+
+    public Network(String url, Activity activity, TextView showResultHere) {
+        this.activity = activity;
+        this.view = showResultHere;
+        this.url = url;
+    }
+
+    public void readResource(){
+        new Thread() {
+            @Override
+            public void run() {
+                String path = url;
+                URL u = null;
+                try {
+                    u = new URL(path);
+                    HttpURLConnection c = (HttpURLConnection) u.openConnection();
+                    c.setRequestMethod("GET");
+                    c.connect();
+                    InputStream in = c.getInputStream();
+                    final ByteArrayOutputStream bo = new ByteArrayOutputStream();
+                    byte[] buffer = new byte[1024];
+                    in.read(buffer); // Read from Buffer.
+                    bo.write(buffer); // Write Into Buffer.
+
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            view.setText(bo.toString());
+                            try {
+                                bo.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
+
+    }
+}
